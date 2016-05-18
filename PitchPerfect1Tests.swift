42fdafb830d6a615592c8fba9c89a4db68bee7bf
pamela032709 <pamela100101@gmail.com>
2016-05-18 17:38:36 -0400

    Add files via upload

---
+++ PitchPerfect1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PitchPerfect1Tests.swift
+//  PitchPerfect1Tests
+//
+//  Created by POCH on 5/15/16.
+//  Copyright © 2016 POCH. All rights reserved.
+//
+
+import XCTest
+@testable import PitchPerfect1
+
+class PitchPerfect1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
